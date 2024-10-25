6bc58fbad5965f2ab24f1a75f721212f013399e1
pejer28 <220711972@students.uajy.ac.id>
2024-10-25 21:41:17 +0700

    Update MainStreamlit_B_pytorch.py

--- MainStreamlit_B_pytorch.py
+++ MainStreamlit_B_pytorch.py
@@ -102,10 +102,10 @@ if selected == 'Klasifikasi':
             input_hasstorage_No = 1
 
         
-        input_data = [[input_hasyard_Yes, input_hasyard_No, input_haspool_Yes, input_haspool_No, input_isnewbuilt_New, input_isnewbuilt_Old, 
+        input_data = np.array([input_hasyard_Yes, input_hasyard_No, input_haspool_Yes, input_haspool_No, input_isnewbuilt_New, input_isnewbuilt_Old, 
                         input_hasstormprotector_Yes, input_hasstormprotector_No, input_hasstorage_Yes, input_hasstorage_No,
                         squaremeters, numberofrooms, floors, citycode, citypartrange, numprevowners,
-                        made, basement, attic, garage, hasguestroom]]
+                        made, basement, attic, garage, hasguestroom]).reshape(1, -1)
         
         # input_data_scaled = scaler.transform([input_data])
         # input_data_selected = feature_selector.transform(input_data_scaled)
