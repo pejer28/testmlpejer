ab271a953338617ebdb9cbd8e1a1893587fad1b9
pejer28 <220711972@students.uajy.ac.id>
2024-10-25 20:40:10 +0700

    Update MainStreamlit_B_pytorch.py

--- MainStreamlit_B_pytorch.py
+++ MainStreamlit_B_pytorch.py
@@ -98,7 +98,7 @@ if selected == 'Klasifikasi':
         
         # input_data_scaled = scaler.transform([input_data])
         # input_data_selected = feature_selector.transform(input_data_scaled)
-
+        print(f"Jumlah fitur input_data: {input_data.shape[1]}")  # Harus 21
         if st.button("Prediksi"):
             GBT_model_prediction = GBT_model.predict(input_data)
             outcome = {0: 'Basic', 1: 'Middle', 2:'Luxury'}
