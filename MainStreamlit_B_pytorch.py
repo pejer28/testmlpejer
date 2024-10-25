523e39599ac1c683c9db998393a73a330e0e1845
pejer28 <220711972@students.uajy.ac.id>
2024-10-25 22:48:54 +0700

    Update MainStreamlit_B_pytorch.py

--- MainStreamlit_B_pytorch.py
+++ MainStreamlit_B_pytorch.py
@@ -113,9 +113,9 @@ if selected == 'Klasifikasi':
         print(f"Jumlah fitur input_data: {input_data.shape[1]}")  # Harus 21
         print(sklearn.__version__)
         if st.button("Prediksi", key="klasifikasi_prediksi"):
-            GBT_model_prediction = GBT_model.predict(input_data)
+           GBT_model_prediction = GBT_model.predict(input_data)
             try:
-                prediksi_kategori = int(GBT_model_prediction[0])
+                prediksi_kategori = int(round(float(GBT_model_prediction[0])))  # Bulatkan ke integer
                 outcome = {0: 'Basic', 1: 'Middle', 2: 'Luxury'}
                 if prediksi_kategori in outcome:
                     st.write(f"Bangunan tersebut masuk ke Kategori: **{outcome[prediksi_kategori]}**")
